c4135ba4b569841b022d7655a87fe52d49983626
Map4che <juan.londono.mt@usa.edu.co>
2022-10-13 00:02:38 -0500

    Cinema Pixel

--- CinemaCiclo/src/main/resources/static/funciones/client.js
+++ CinemaCiclo/src/main/resources/static/funciones/client.js
@@ -3,4 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
+function leerClientes() {
+    //Funcion GET
 
+ $.ajax({
+        url: 'api/Client/all',
+        type: 'GET',
+        dataType: 'json',
+
+        success: function (clientes) {
+            let cs = clientes.items;
+            $("#listaClientes").empty();
+            for (i=0;i<cs.length;i++){
+                $("#listaClientes").append("<b>"+cs[i].id +"."+"</b>" +"<b>" + cs[i].name + "</b> " + cs[i].correo);
+               $("#listaClientes").append("<button onclick='eliminarCLiente("+cs[i].id+")'>Borrar</button><br>");
+            }
+        },
+        error: function (xhr, status) {
+            alert('Ha sucedido un error');
+        }
+
+
+    });
+}
+function llamarInformacion(){
+    $.ajax({
+        url:"https://gd8153a19b5daf4-hernanapexbd.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/cinema/cinema",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta.items)
+        }
+        
+    });
+}
