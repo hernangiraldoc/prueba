f3f294da1d70e88d798e509ec8f5a27bec2a644f
Map4che <juan.londono.mt@usa.edu.co>
2022-10-15 01:26:08 -0500

    FInalizacion Reto 5!!

--- CinemaCiclo/src/main/java/com/CinemaCiclo/CinemaCiclo/model/Reservation.java
+++ CinemaCiclo/src/main/java/com/CinemaCiclo/CinemaCiclo/model/Reservation.java
@@ -31,7 +31,7 @@ public class Reservation implements Serializable {
     private Integer idReservation;
     private Date startDate;
     private Date devolutionDate;
-    private String status="created";
+    private String status;
     
 
     @ManyToOne
